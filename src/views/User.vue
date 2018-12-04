5e8f6ea0b47c7b3f1c918c7876c46f2cc1605315
Brendan McDonald <brendan.mcdonald@eagledream.com>
2018-12-04 17:33:00 -0500

    initial commit

---
+++ src/views/User.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <pre>{{user}}</pre>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "User",
+  props: {
+    id: Number
+  },
+  data() {
+    return {
+      user: {}
+    };
+  },
+  mounted() {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${this.id}`)
+      .then(res => {
+        this.user = res.data;
+      })
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <li v-for="user in users" :key="user.id">
+      <router-link :to="'/users/' + user.id">{{user.name}}</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: []
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        this.users = res.data;
+      })
+      .catch(err => console.log(err));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
